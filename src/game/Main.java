3746fe2d31796d27ccb71b9516ff4d5c15792cb9
Pablo87996 <pablosantoscerqueira@gmail.com>
2025-07-13 14:02:39 -0300

    fix: multiple sceneries were been created on Main

--- src/game/Main.java
+++ src/game/Main.java
@@ -5,10 +5,6 @@ import jplay.Window;
 public class Main{
     public static void main(String[] args) {
         Window window = new Window(1024, 768);
-
-        while(true){
-            window.update();
-            new Scenery(window);
-        }
+        new Scenery(window);
     }
 }
